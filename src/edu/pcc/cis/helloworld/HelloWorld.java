9ef0c93acc7e3ebc6f3b2fe1ddbb74cf8ee31e71
Travis Jackson <travisjackson3@pcc.edu>
2018-02-06 19:37:33 -0800

    added print date

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -1,9 +1,13 @@
 package edu.pcc.cis.helloworld;
 
+import java.util.Date;
+
 public class HelloWorld {
     public static void main(String[] args){
         System.out.println("Hello World");
         System.out.println("This is Travis Jackson's Assignment 4");
+
+        System.out.println(new Date());
 
     }
 
