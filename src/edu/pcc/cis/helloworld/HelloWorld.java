c6f6359a92b3fe813ca958d76d261d7e45b5d392
Travis Jackson <travisjackson3@pcc.edu>
2018-02-06 19:35:46 -0800

    added main method and print statment

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -1,4 +1,10 @@
 package edu.pcc.cis.helloworld;
 
 public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello World");
+        System.out.println("This is Travis Jackson's Assignment 4");
+
+    }
+
 }
